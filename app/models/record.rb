066467c33e590ecd4db9c045212ca39b6c1ff1d2
Andrew Fader <andrevan@gmail.com>
2016-05-12 18:55:21 -0400

    csv parse

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,8 +1,15 @@
+require 'csv'
 class Record < ActiveRecord::Base
-
   def self.parse_from_csv
-    %s(first_name last_name middle_name name_suffix raddnumber rhalfcode rapartment pre street post city zip5 zip4 add1 add2 add3 add4 dob gender party otherparty county ed ld town ward cd sd ad lastvoted prevyearvoted prevcounty prevaddress prevname countynum regdate appsource idrequired idmet status reason inact_date purge_date last_name middle_name name_suffix raddnumber rhalfcode rapartment pre street post city zip5 zip4 add1 add2 add3 add4 dob gender party otherparty county ed ld town ward cd sd ad lastvoted prevyearvoted prevcounty prevaddress prevname countynum regdate appsource idrequired idmet status reason inact_date purge_date voterid history).each do |col|
-      binding.pry
+    fields = %w(last_name first_name middle_name name_suffix raddnumber rhalfcode rapartment pre street post city zip5 zip4 add1 add2 add3 add4 dob gender party otherparty county ed ld town ward cd sd ad lastvoted prevyearvoted prevcounty prevaddress prevname countynum regdate appsource idrequired idmet status reason inact_date purge_date voterid history)
+    CSV.foreach(Rails.root.join('data.csv')).with_index do |row, index|
+      puts index
+      record = Record.new
+      fields.each_with_index do |field, val|
+        record.send("#{field}=", row[val])
+      end
+      puts record.first_name + record.last_name
+      record.save!
     end
   end
 end
